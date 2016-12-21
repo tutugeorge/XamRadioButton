52338ada1a475690a8eb2d39bc16f33b34251620
tutugeorge <tutugeorge@gmail.com>
2016-12-21 23:31:09 +0530

    Added events for TabGrid

--- RadioButton/MySwipeTab.xaml.cs
+++ RadioButton/MySwipeTab.xaml.cs
@@ -10,6 +10,31 @@ namespace RadioButton
 		public MySwipeTab()
 		{
 			InitializeComponent();
+			tab.TabChanged += (sender, e) => { foo(sender, e); };
+			tab.ItemSource = new List<Boo>()
+			{
+				new Boo(){ Obj1 = "one", Obj2 = 1},
+				new Boo(){ Obj1 = "two", Obj2 = 2}
+			};
+		}
+
+		void foo(object sender, EventArgs e)
+		{
+			tab.TabLayout = new SwipeFrame() { BackgroundColor = Color.Purple };
+		}
+	}
+
+	public class Boo
+	{
+		public string Obj1
+		{
+			get;
+			set;
+		}
+		public int Obj2
+		{
+			get;
+			set;
 		}
 	}
 }
--- RadioButton/TabView.cs
+++ RadioButton/TabView.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace RadioButton
@@ -17,24 +19,78 @@ namespace RadioButton
 			});
 			InitHeaderLayout();
 			InitBodyLayout();
+			AddHeaderTabs();
 		}
 
-		public static readonly BindableProperty HeaderHeightProperty =
-			BindableProperty.Create(
-				propertyName: "HeaderHeight",
-				returnType: typeof(void),
-				declaringType: typeof(TabView),
-				propertyChanged: (bindable, oldValue, newValue) => ((TabView)bindable).OnHeaderHeightChanged(newValue)
-			);
-		private void OnHeaderHeightChanged(object newValue)
+		public event EventHandler TabChanged;
+		public void OnTabChanged()
 		{
+			if (TabChanged != null)
+				TabChanged(this, null);
+		}
+
+		private SwipeFrame _tabLayout;
+		public SwipeFrame TabLayout
+		{
+			set 
+			{ 
+				_tabLayout = value;
+				UpdateBodyLayout();
+			}
+		}
+
+		private IEnumerable _itemSource;
+		public IEnumerable ItemSource
+		{
+			get { return _itemSource; }
+			set
+			{
+				_itemSource = value;
+				UpdateLayout();
+			}
+		}
+
+		private void UpdateLayout()
+		{
+			if (_itemSource != null)
+			{
+				foreach (var item in _itemSource)
+				{
+					var type = item.GetType();
+
+				}
+			}
+		}
+
+		//public static readonly BindableProperty HeaderHeightProperty =
+		//	BindableProperty.Create(
+		//		propertyName: "HeaderHeight",
+		//		returnType: typeof(void),
+		//		declaringType: typeof(TabView),
+		//		propertyChanged: (bindable, oldValue, newValue) => ((TabView)bindable).OnHeaderHeightChanged(newValue)
+		//	);
+		//private void OnHeaderHeightChanged(object newValue)
+		//{
 			
+		//}
+
+		private void AddHeaderTabs()
+		{
+			var headerLayout = Children[0] as StackLayout;
+			var btn1 = new TabButton() { Text = "Btn1", BackgroundColor = Color.Accent };
+			var btn2 = new TabButton() { Text = "Btn2", BackgroundColor = Color.Accent };
+			var btn3 = new TabButton() { Text = "Btn3", BackgroundColor = Color.Accent };
+			headerLayout.Children.Add(btn1);
+			headerLayout.Children.Add(btn2);
+			headerLayout.Children.Add(btn3);
 		}
 
 		private void InitHeaderLayout()
 		{
 			var layout = new StackLayout();
 			layout.BackgroundColor = Color.Gray;
+			layout.Orientation = StackOrientation.Horizontal;
+			layout.HorizontalOptions = LayoutOptions.CenterAndExpand;
 			Children.Add(layout, 0, 0);
 		}
 
@@ -45,5 +101,38 @@ namespace RadioButton
 			Children.Add(layout, 0, 1);
 		}
 
+		private void UpdateBodyLayout()
+		{
+			Children.RemoveAt(1);
+			Children.Add(_tabLayout, 0, 1);
+		}
+	}
+
+	public class TabButton : Button
+	{
+		public TabButton()
+		{
+			this.Clicked += (sender, e) => { Tapped(); };
+		}
+
+		void Tapped()
+		{
+			var stackLayout = this.Parent as StackLayout;
+			var tab = stackLayout.Parent as TabView;
+			foreach (var child in stackLayout.Children)
+			{
+				(child as TabButton).IsSelected = false;
+				(child as TabButton).BackgroundColor = Color.Silver;
+			}
+			this.IsSelected = true;
+			this.BackgroundColor = Color.White;
+			tab.OnTabChanged();
+		}
+
+		public bool IsSelected
+		{
+			get;
+			set;
+		}
 	}
 }
